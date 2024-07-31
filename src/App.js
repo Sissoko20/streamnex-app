5c055ddcb2c92b62de03e35eed1ef3301a792b6c
Sissoko Modibo <m.sissoko.7035@gmail.com>
2024-07-31 12:51:37 +0000

    Intération de SpeedInsights pour performance

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Movies from "./Components/Movies";
 import TvShows from "./Components/TvShows";
 import History from "./Components/History";
 import Setting from "./Components/Settings";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const { Sider, Content } = Layout;
 
@@ -58,124 +59,119 @@ const App = () => {
   };
 
   return (
-    <> 
-   
-
-<Layout style={{ height: "100vh" }}>
-   <div className="BlueItem">
-
-
-    </div>
-<Sider
-  theme="dark"
-  width={200}
-  collapsible
-  collapsed={collapsed}
-  onCollapse={() => setCollapsed(!collapsed)}
-  style={{
-    overflow: "auto",
-    height: "100vh",
-    position: "fixed",
-    left: 0,
-    paddingTop: "20vh",
-    textAlign: "center",
-  }}
->
-  <Button
-    type="primary"
-    onClick={() => setCollapsed(!collapsed)}
-    style={{
-      marginBottom: 16,
-      background: "none",
-      borderBottom: "2px solid white",
-    }}
-  >
-    {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-  </Button>
-  <Menu
-    theme="dark"
-    mode="inline"
-    className="menuItem"
-    defaultSelectedKeys={["5"]}
-    selectedKeys={[selectedKey]}
-    onClick={(e) => setSelectedKey(e.key)}
-  >
-    <Menu.Item
-      key="1"
-      icon={
-        <SearchOutlined
+    <>
+      <Layout style={{ height: "100vh" }}>
+        <Sider
+          theme="dark"
+          width={200}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={() => setCollapsed(!collapsed)}
           style={{
-            borderBottom: "2px solid white",
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+            paddingTop: "20vh",
+            textAlign: "center",
           }}
-        />
-      }
-      style={menuItemStyle}
-    >
-      Search
-    </Menu.Item>
-    <Menu.Item key="2" icon={<GlobalOutlined />} style={menuItemStyle}>
-      Country
-    </Menu.Item>
-    <Menu.Item
-      key="3"
-      icon={
-        <AppstoreOutlined
-          style={{
-            borderBottom: "2px solid white",
-          }}
-        />
-      }
-      style={menuItemStyle}
-    >
-      Category
-    </Menu.Item>
-    <Menu.Item
-      key="4"
-      icon={<VideoCameraOutlined />}
-      style={menuItemStyle}
-    >
-      LiveTV
-    </Menu.Item>
-    <Menu.Item
-      key="5"
-      icon={
-        <PlayCircleOutlined
-          style={{
-            borderBottom: "2px solid white",
-          }}
-        />
-      }
-      style={menuItemStyle}
-    >
-      Movies
-    </Menu.Item>
-    <Menu.Item
-      key="6"
-      icon={<VideoCameraOutlined />}
-      style={menuItemStyle}
-    >
-      TvShows
-    </Menu.Item>
-    <Menu.Item key="7" icon={<AudioOutlined />} style={menuItemStyle}>
-      Radio
-    </Menu.Item>
-    <div className="historySettings" style={{ paddingTop: "10vh" }}>
-      <Menu.Item key="8" icon={<HistoryOutlined />} style={menuItemStyle}>
-        History
-      </Menu.Item>
-      <Menu.Item key="9" icon={<SettingOutlined />} style={menuItemStyle}>
-        Setting
-      </Menu.Item>
-    </div>
-  </Menu>
-</Sider>
-<Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
-  <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-    {renderContent()}
-  </Content>
-</Layout>
-</Layout>
-</>
+        >
+          <Button
+            type="primary"
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              marginBottom: 16,
+              background: "none",
+              borderBottom: "2px solid white",
+            }}
+          >
+            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          </Button>
+          <Menu
+            theme="dark"
+            mode="inline"
+            className="menuItem"
+            defaultSelectedKeys={["5"]}
+            selectedKeys={[selectedKey]}
+            onClick={(e) => setSelectedKey(e.key)}
+          >
+            <Menu.Item
+              key="1"
+              icon={
+                <SearchOutlined
+                  style={{
+                    borderBottom: "2px solid white",
+                  }}
+                />
+              }
+              style={menuItemStyle}
+            >
+              Search
+            </Menu.Item>
+            <Menu.Item key="2" icon={<GlobalOutlined />} style={menuItemStyle}>
+              Country
+            </Menu.Item>
+            <Menu.Item
+              key="3"
+              icon={
+                <AppstoreOutlined
+                  style={{
+                    borderBottom: "2px solid white",
+                  }}
+                />
+              }
+              style={menuItemStyle}
+            >
+              Category
+            </Menu.Item>
+            <Menu.Item
+              key="4"
+              icon={<VideoCameraOutlined />}
+              style={menuItemStyle}
+            >
+              LiveTV
+            </Menu.Item>
+            <Menu.Item
+              key="5"
+              icon={
+                <PlayCircleOutlined
+                  style={{
+                    borderBottom: "2px solid white",
+                  }}
+                />
+              }
+              style={menuItemStyle}
+            >
+              Movies
+            </Menu.Item>
+            <Menu.Item
+              key="6"
+              icon={<VideoCameraOutlined />}
+              style={menuItemStyle}
+            >
+              TvShows
+            </Menu.Item>
+            <Menu.Item key="7" icon={<AudioOutlined />} style={menuItemStyle}>
+              Radio
+            </Menu.Item>
+            <div className="historySettings" style={{ paddingTop: "10vh" }}>
+              <Menu.Item key="8" icon={<HistoryOutlined />} style={menuItemStyle}>
+                History
+              </Menu.Item>
+              <Menu.Item key="9" icon={<SettingOutlined />} style={menuItemStyle}>
+                Setting
+              </Menu.Item>
+            </div>
+          </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: collapsed ? 80 : 200 }}>
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            {renderContent()}
+            <SpeedInsights />
+          </Content>
+        </Layout>
+      </Layout>
+    </>
   );
 };
 
