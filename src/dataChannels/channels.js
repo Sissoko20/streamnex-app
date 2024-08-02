03c951c0d502ada6d6b9dac4f042a2d2a961e130
Sissoko Modibo <m.sissoko.7035@gmail.com>
2024-08-02 21:12:56 +0000

    Adding channel ci to channels

--- src/dataChannels/channels.js
+++ src/dataChannels/channels.js
@@ -95,5 +95,93 @@ export const channels = [
     {
         "name": "TPMP (1080p)",
         "url": "https://m3u.globaliptv.tv/TPMP.m3u8"
-    }
+    },
+    
+        {
+            "name": "Afroculture TV (1080p)",
+            "url": "https://helga.iptv2022.com/sh/Afroculture/index.m3u8"
+        },
+        {
+            "name": "Alpha et Omega TV (720p)",
+            "url": "https://video1.getstreamhosting.com:1936/8318/8318/playlist.m3u8"
+        },
+        {
+            "name": "B\u00c2TIR-TV (576p)",
+            "url": "https://webstreaming.viewmedia.tv/web_021/Stream/playlist.m3u8"
+        },
+        {
+            "name": "Benie TV (720p)",
+            "url": "https://voozmedia.fun/benietv/livestream/playlist.m3u8"
+        },
+        {
+            "name": "Christ Live (720p)",
+            "url": "https://voozmedia.fun/stream/christlive/playlist.m3u8"
+        },
+        {
+            "name": "Divin Amour TV (720p)",
+            "url": "https://voozmedia.fun/divinamourtv/livestream/playlist.m3u8"
+        },
+        {
+            "name": "Ecclesia TV (720p)",
+            "url": "https://helga.iptv2022.com/sh/Ecclesia_TV/index.m3u8"
+        },
+        {
+            "name": "Esp\u00e9rance TV (720p)",
+            "url": "https://ik084235.ikoula.com/hls/Diff@mobile1.m3u8"
+        },
+        {
+            "name": "Golden Sweet TV (720p)",
+            "url": "https://helga.iptv2022.com/sh/GOLDEN_SWEET_TV/index.m3u8"
+        },
+        {
+            "name": "Guide Love TV (720p)",
+            "url": "https://video1.getstreamhosting.com:1936/8056/8056/playlist.m3u8"
+        },
+        {
+            "name": "Ivoire Channel (720p)",
+            "url": "https://video1.getstreamhosting.com:1936/8244/8244/playlist.m3u8"
+        },
+        {
+            "name": "LMTV French (720p) [Not 24/7]",
+            "url": "https://video1.getstreamhosting.com:1936/8282/8282/playlist.m3u8"
+        },
+        {
+            "name": "Miracle TV+ (720p)",
+            "url": "https://strhlslb01.streamakaci.tv/str_mtv_mtv/str_mtv_multi/playlist.m3u8"
+        },
+        {
+            "name": "NCI (1080p)",
+            "url": "https://pixa.cdn.easybroadcast.fr/nci/nci.isml/master.m3u8"
+        },
+        {
+            "name": "Novela Channel (720p) [Not 24/7]",
+            "url": "https://video1.getstreamhosting.com:1936/8284/8284/playlist.m3u8"
+        },
+        {
+            "name": "NTV Afrique (1080p) [Not 24/7]",
+            "url": "https://strhlslb01.streamakaci.tv/str_ntv_ntv/str_ntv_ntv_multi/playlist.m3u8"
+        },
+        {
+            "name": "REFLET TV (1080p)",
+            "url": "https://edge-a3.evrideo.tv/8f37c9f0-fe22-44f4-b64a-76ad11730daf_1000026630_HLS/manifest.m3u8"
+        },
+        {
+            "name": "RTI 1 (1080p) [Not 24/7]",
+            "url": "https://www.enovativecdn.com/rticdn/smil:rti1.smil/playlist.m3u8"
+        },
+        {
+            "name": "RTI 2 (1080p) [Not 24/7]",
+            "url": "https://www.enovativecdn.com/rticdn/smil:rti2.smil/playlist.m3u8"
+        },
+        {
+            "name": "RTVC (720p)",
+            "url": "https://cdn140m.panaccess.com/HLS/RTVC/index.m3u8"
+        },
+        {
+            "name": "Passion Novelas",
+            "url": "https://d219tvyafu2vka.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-csm4hzxgjueax/index.m3u8"
+        }
+    
+
+    
 ];
