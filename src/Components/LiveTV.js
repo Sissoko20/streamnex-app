f9a8000bca664ebb3436f0befe77558a32a21de0
Sissoko Modibo <m.sissoko.7035@gmail.com>
2024-08-03 15:32:59 +0000

    desactivation de position dans le className video-js vjs-default-skin

--- src/Components/LiveTV.js
+++ src/Components/LiveTV.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef, Suspense } from 'react';
-import 'video.js/dist/video-js.css';
-import videojs from 'video.js';
-import { Row, Col, message, Spin } from 'antd';
-import 'antd/dist/reset.css'; // Assurez-vous d'importer le style d'Ant Design
-import { channels } from '../dataChannels/channels'; // Importer les chaînes
-import '../Components/LiveTV.css';
-const ChannelCard = React.lazy(() => import('./ChannelCard'));
+import React, { useState, useEffect, useRef, Suspense } from "react";
+import "video.js/dist/video-js.css";
+import videojs from "video.js";
+import { Row, Col, message, Spin } from "antd";
+import "antd/dist/reset.css"; // Assurez-vous d'importer le style d'Ant Design
+import { channels } from "../dataChannels/channels"; // Importer les chaînes
+import "../Components/LiveTV.css";
+const ChannelCard = React.lazy(() => import("./ChannelCard"));
 
 const LiveTV = () => {
   const playerRef = useRef(null);
@@ -17,7 +17,7 @@ const LiveTV = () => {
       const videoPlayer = videojs(playerRef.current, {
         controls: true,
         autoplay: false,
-        preload: 'auto'
+        preload: "auto",
       });
       setPlayer(videoPlayer);
     }
@@ -32,10 +32,10 @@ const LiveTV = () => {
   // Vérifie la disponibilité d'une URL
   const checkUrlAvailability = async (url) => {
     try {
-      const response = await fetch(url, { method: 'HEAD' });
+      const response = await fetch(url, { method: "HEAD" });
       return response.ok;
     } catch (error) {
-      console.error('Error checking URL availability:', error);
+      console.error("Error checking URL availability:", error);
       return false;
     }
   };
@@ -44,24 +44,31 @@ const LiveTV = () => {
     if (player) {
       if (await checkUrlAvailability(url)) {
         try {
-          player.src({ src: url, type: 'application/x-mpegURL' });
+          player.src({ src: url, type: "application/x-mpegURL" });
           await player.play();
         } catch (error) {
-          message.error('Erreur lors de la lecture de la chaîne. Veuillez réessayer.');
-          console.error('Error playing the channel:', error);
+          message.error(
+            "Erreur lors de la lecture de la chaîne. Veuillez réessayer."
+          );
+          console.error("Error playing the channel:", error);
         }
       } else {
-        message.error('Chaîne non disponible.');
+        message.error("Chaîne non disponible.");
       }
     }
   };
 
   return (
     <div>
-      <div className='video-player'>
-        <video ref={playerRef} className="video-js vjs-default-skin" controls />
+      <div className="video-player">
+        <video
+          ref={playerRef}
+          className="video-js vjs-default-skin"
+          controls
+          style={{}}
+        />
       </div>
-      <div className='channel-list'>
+      <div className="channel-list">
         <h3>Liste des chaines</h3>
         {loading ? (
           <Spin tip="Chargement des chaînes..." />
@@ -70,7 +77,10 @@ const LiveTV = () => {
             <Suspense fallback={<Spin tip="Chargement des chaînes..." />}>
               {channels.map((channel) => (
                 <Col xs={24} sm={12} md={8} key={channel.name}>
-                  <ChannelCard channel={channel} onClick={() => handleChannelChange(channel.url)} />
+                  <ChannelCard
+                    channel={channel}
+                    onClick={() => handleChannelChange(channel.url)}
+                  />
                 </Col>
               ))}
             </Suspense>
